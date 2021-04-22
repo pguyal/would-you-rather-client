39bd022c94ba0335d11f2008fcfc51d50cbedbb7
pguyal <pguyal@hotmail.com>
2021-04-22 15:04:59 -0400

    Add submit events for sign up, sign in, and change password, also add click events for sign out and change password, and add hide events for sign out, change password form and button

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,17 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const authEvents = require('./auth/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#sign-out').hide()
+  $('#change-pw-form').hide()
+  $('#change-pw-button').hide()
+  $('#sign-up').on('submit', authEvents.onSignUp)
+  $('#sign-in').on('submit', authEvents.onSignIn)
+  $('#sign-out').on('click', authEvents.onSignOut)
+  $('change-pw-form').on('submit', authEvents.onConfirmChangePw)
+  $('#change-pw-button').on('click', authEvents.onChangePw)
 })
