3fda10cce2e30f4d90f8b205735153a469d43041
pguyal <pguyal@hotmail.com>
2021-04-23 14:51:26 -0400

    Complete all authentication features

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -1,1 +1,73 @@
 const store = require('../store')
+
+const onSignUpSuccess = function () {
+  $('#message').text('Account successfully created!')
+  $('#message').addClass('success')
+  setTimeout(() => {
+    $('#message').text('')
+    $('#message').removeClass('success')
+  }, 2000)
+  $('form').trigger('reset')
+}
+
+const onSignInSuccess = function (response) {
+  store.user = response.user
+  $('#message').text(response.user.email + ' signed in successfully')
+  $('#message').addClass('success')
+  setTimeout(() => {
+    $('#message').text('')
+    $('#message').removeClass('success')
+  }, 2000)
+  $('form').trigger('reset')
+  $('#sign-out').show()
+  $('#change-pw-button').show()
+  $('#sign-up').hide()
+  $('#sign-in').hide()
+}
+
+const onChangePasswordSuccess = function () {
+  $('#message').text('Password changed successfully')
+  $('#message').addClass('success')
+  setTimeout(() => {
+    $('#message').text('')
+    $('#message').removeClass('success')
+  }, 2000)
+  $('form').trigger('reset')
+  $('#change-password').hide()
+  $('#return').hide()
+  $('#change-pw-button').show()
+  $('#sign-out').show()
+}
+
+const onSignOutSuccess = function () {
+  store.user = null
+  $('#message').text('Signed out successfully')
+  $('#message').addClass('success')
+  setTimeout(() => {
+    $('#message').text('')
+    $('#message').removeClass('success')
+  }, 2000)
+  $('form').trigger('reset')
+  $('#change-pw-button').hide()
+  $('#sign-out').hide()
+  $('#sign-up').show()
+  $('#sign-in').show()
+}
+
+const onError = function () {
+  $('#message').text('Something went wrong, please try again.')
+  $('#message').addClass('failure')
+  setTimeout(() => {
+    $('#message').text('')
+    $('#message').removeClass('failure')
+  }, 5000)
+  $('form').trigger('reset')
+}
+
+module.exports = {
+  onSignUpSuccess,
+  onSignInSuccess,
+  onSignOutSuccess,
+  onChangePasswordSuccess,
+  onError
+}
