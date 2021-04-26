c9d8e26d96d55dcc4c55ac72bef8e00a55d17826
pguyal <pguyal@hotmail.com>
2021-04-26 14:54:54 -0400

    Create ui code for successful delete question message

--- assets/scripts/question/ui.js
+++ assets/scripts/question/ui.js
@@ -7,7 +7,7 @@ const onCreateQuestionSuccess = function (response) {
   setTimeout(() => {
     $('#question-message').text('')
     $('#question-message').removeClass('success')
-  }, 5000)
+  }, 3000)
   $('form').trigger('reset')
 }
 
@@ -30,16 +30,34 @@ const onViewQuestionSuccess = function (response) {
   let questionsHtml = ''
   questions.forEach(question => {
     questionsHtml += `
+    <hr>
     <h4>Question: ${question.text}</h4>
     <p>Option one: ${question.option_one}</p>
     <p>Option two: ${question.option_two}</p>
+    <button class='questions-destroy-dynamic' data-id=${question._id}>
+        Delete Question
+      </button>
+    <hr>
     `
   })
   $('#questions-display').html(questionsHtml)
+  // if (questionsHtml === ' ') {
+  //   $('#questions-display').html('You have no questions!')
+  // }
+}
+
+const onDestroyQuestionSuccess = function () {
+  $('#question-delete-message').text('Question deleted successfully')
+  $('#question-delete-message').addClass('success')
+  setTimeout(() => {
+    $('#question-delete-message').text('')
+    $('#question-delete-message').removeClass('success')
+  }, 3000)
 }
 
 module.exports = {
   onCreateQuestionSuccess,
   onQuestionError,
-  onViewQuestionSuccess
+  onViewQuestionSuccess,
+  onDestroyQuestionSuccess
 }
